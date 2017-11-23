e6904da88011fb597d88376288890784bbac935c
jameskun77 <wangchao104999@126.com>
2017-11-23 18:17:45 +0800

    添加错误检查函数

--- graceCamera/app/src/main/java/com/example/gracecamera/Util/LoggerConfig.java
+++ graceCamera/app/src/main/java/com/example/gracecamera/Util/LoggerConfig.java
@@ -1,9 +1,24 @@
 package com.example.gracecamera.Util;
 
+import android.util.Log;
+
+import static android.opengl.GLES20.GL_NO_ERROR;
+import static android.opengl.GLES20.glGetError;
+
 /**
  * Created by 123 on 2017/11/19.
  */
 
 public class LoggerConfig {
     public static final boolean ON = true;
+
+    private static final String TAG = "checkGLError";
+
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = glGetError()) != GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            //throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
 }

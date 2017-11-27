acc6ef69944e8b7b742e2e9a5f25b453e4ba409e
jameskun77 <wangchao104999@126.com>
2017-11-27 12:16:01 +0800

    去除练习项目

--- RenderTexture/app/src/main/assets/squareFragmentShader.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-
-uniform sampler2D uTextureUnit;
-varying vec2 vTextureCoordinates;
-
-void main()
-{
-    gl_FragColor = texture2D(uTextureUnit, vTextureCoordinates);
-}
--- RenderTexture/app/src/main/java/com/example/rendertexture/Util/LoggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.rendertexture.Util;
-
-/**
- * Created by 123 on 2017/11/19.
- */
-
-public class LoggerConfig {
-    public static final boolean ON = true;
-}
